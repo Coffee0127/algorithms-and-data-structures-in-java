e59cb9e6c78c722798858155b281ebe8b2e19586
Bo-Xuan Fan <coffee0127@gmail.com>
2018-12-07 15:13:55 +0800

    Add class: HeapNode main method

---
+++ src/io/github/coffee0127/heap/node/MainHeapNode.java
@@ -0,0 +1,52 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Bo-Xuan Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.github.coffee0127.heap.node;
+
+/**
+ * MainHeapNode
+ *
+ * @author Bo-Xuan Fan
+ * @since 2018-12-05
+ */
+public class MainHeapNode {
+
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        heap.addNode(0);
+        heap.addNode(1);
+        heap.addNode(2);
+        heap.addNode(3);
+        heap.addNode(4);
+        heap.addNode(5);
+        heap.addNode(6);
+        heap.addNode(8);
+        heap.addNode(7);
+
+        heap.postOrderPrint(heap.getHeapNode());
+        heap.deleteNode(heap.getHeapNode(), 2);
+        System.out.println("------------------");
+        heap.preOrderPrint(heap.getHeapNode());
+    }
+
+}
