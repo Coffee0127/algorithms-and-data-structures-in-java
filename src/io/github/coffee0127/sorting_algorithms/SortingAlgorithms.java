99e8a07b82bdb12827cb2d9c4e8e4a313bbb783d
Bo-Xuan Fan <coffee0127@gmail.com>
2018-12-08 17:47:26 +0800

    implement selectionSort

--- src/io/github/coffee0127/sorting_algorithms/SortingAlgorithms.java
+++ src/io/github/coffee0127/sorting_algorithms/SortingAlgorithms.java
@@ -44,6 +44,22 @@ public class SortingAlgorithms {
         }
     }
 
+    public void selectionSort() {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap the min value
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
     @Override
     public String toString() {
         return Arrays.toString(array);
